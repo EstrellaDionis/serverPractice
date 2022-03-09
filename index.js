79ffd460e78eda0e99cb545588a39f7c8ee80736
EstrellaDionis <EstrellaDionis@gmail.com>
2022-03-09 18:39:31 -0500

    added some notes for portion about express path and parameters

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { response } = require("express");
 const express = require("express");
 const app = express();
-console.dir(app);
+// console.dir(app);
 
 // app.use((req, res) => {
 //   console.log("We got a new request!");
@@ -12,12 +12,18 @@ app.get("/", (req, res) => {
   res.send("This is the home page!");
 });
 
+//req.params is specifically the subreddit that we're searching for.
+//you can see everything inside of the req object with console.log(req)
 app.get("/r/:subreddit", (req, res) => {
+  console.log(req.params);
   const { subreddit } = req.params;
   res.send(`<h1>Browsing the ${subreddit} subreddit`);
 });
 
+//the colon is declaring a VARIABLE. These can be named ANYTHING BUT,
+//as always, we want to name them things related to what we want!
 app.get("/r/:subreddit/:postId", (req, res) => {
+  console.log(req.params);
   const { subreddit, postId } = req.params;
   res.send(
     `<h1>Viewing Post ID: ${postId} on the Subreddit: ${subreddit} subreddit`
